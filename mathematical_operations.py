0ade1d9fb8d5e8e1cbb545cb569e1dd67547a2f4
Aayush181509 <aayush.181509@ncit.edu.np>
2024-03-05 10:33:36 +0545

    Remaining 2 functions added
    Substraction and Multiplication Functions added to the
    existing mathematical_operations.py file

--- mathematical_operations.py
+++ mathematical_operations.py
@@ -1,8 +1,23 @@
-def addition(a,b):
+def add(a,b):
     if isinstance(a,(int,float)):
         return a+b
     else:
-        raise Exception("Enter a number")
+        raise Exception("Enter a numeric value")
+    
+
+def substract(a,b):
+    if isinstance(a,(int,float)):
+        return a-b
+    else:
+        raise Exception("Enter a numeric value")
+    
+
+def multiply(a,b):
+    if isinstance(a,(int,float)):
+        return a*b
+    else:
+        raise Exception("Enter a numeric value")
     
 
 
+print(multiply(4,6),add(4,6),substract(4,6))
