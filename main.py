908c1d46ea3db1206fe1f779f2bd80f1f19bdf7d
Aayush181509 <aayush.181509@ncit.edu.np>
2024-03-05 11:08:20 +0545

    Main.py file created to act as an entry point for our mathematical funcions

---
+++ main.py
@@ -0,0 +1,16 @@
+from mathematical_operations import add,substract,multiply,divide
+
+try:
+    num1 = float(input("Enter a number: "))
+    num2 = float(input("Enter a number: "))
+
+    print("Sum: ",add(num1,num2))
+    print("Difference: ",substract(num1,num2))
+    print("Multiplication: ",multiply(num1,num2))
+    print("Division: ",divide(num1,num2))
+
+except ValueError as e:
+    print("Value Error: ",e)
+
+except Exception as e:
+    print("Exception: ",e)
